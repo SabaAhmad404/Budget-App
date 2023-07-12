0d9f4acd9683b5ac82816ebd3e9918818bdbccaf
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-12 18:05:24 +0500

    update the models with validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_and_belongs_to_many :entities
+
+    validates :name, presence: true
+    validates :icone, presence: true
 end
