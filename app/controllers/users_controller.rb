86497ffbad5d2840f9937440a5f5e9e86ed9085b
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-12 21:46:20 +0500

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
-class Users <ApplicationController
+class Users < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @users=User.all
+    end
+    
+    def show
+    end
+
+    def edit
+    end
+
+    def new
+        @user=User.new
+    end
+
+    def create
+        @user=User.new(user_params)
+        if@user.save
+            redirect_to user_url(@user), notice: 'User was created Successfully'
+        else
+            render :new
+        end
+    end
+
+    def update
+        @user=User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user), notice: 'User was updated Succesfully'
+        else
+            render :edit
+        end
+    end
+
+    def destroy 
+        @user.destroy
+        redirect_to users_url, notice: 'User is destroy successfully'
+    end
+
+    private
+    def set_user
+        @user=User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
