ec6f0d6f48beed41fe7c318a8ba172cfb16f907d
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-13 09:54:05 +0500

    updated all controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,59 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :new, :update, :destroy]
+
+    def index
+        @categories=Category.where(author_id: current_user.id).order(created_at: :desc)
+    end
+    def new
+        @category=Category.new
+    end
+
+    def show
+        @category=Category.includes(:entities).find(params[:id])
+        @entities=@category.entities.order(created_at: :desc)
+    end
+
+    def edit
+    end
+
+    def create
+        @category=Category.new(category_params)
+        @category.author=current_user
+        if @category.save
+            redirect_to categories_path
+            flash[:notice]= "Category was created successfully"
+        else
+            flash[:notice]="couldnt created the category"
+            render :new
+        end
+    end
+
+    def update
+        if @category.update(category_params)
+            redirect_to category_url(@category)
+            flash[:notice]= "category was updated successfully"
+        else
+            flash[:notice]= "created couldnt updated"
+            render :edit
+        end
+    end
+
+
+    def destroy
+        @category=Category.find(params[:id])
+        @category.destroy
+        redirect_to categories_path
+        flash[:notice]="category has been destroy succesfully"
+    end
+
+
+    private 
+    
+    def set_category
+        @categor=Category.find(params[:id])
+    end
+
+    def category_params
+        params.require(:category).permit(:name, :icone).merge(author_id: current_user.id)
+    end
 end
--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -17,22 +17,27 @@ class EntitiesController <ApplicationController
      def create
         @entity=Entity.new(entity_params)
         if @entity.save
-            redirect_to entity_url(@entity), notice: 'Entity created successfully'
+            redirect_to entity_url(@entity)
+            flash[:notice] = 'Entity created successfully'
         else
+           flash[:notice]= 'Entity  couldnt created successfully'
             render :new
         end
      end
 
      def update
         if @entity.update(entity_params)
-            redirect_to entity_url(@entity), notice: "entity was updated successfully"
+            redirect_to entity_url(@entity)
+            flash[:notice]= "entity was updated successfully"
         else
-            render :edit
+           flash[:notice]= "entity couldnt updated successfully"
+           render :edit
      end
 
      def destroy 
         @entity.destroy
-        redirect_to entities_url, notice: 'entity was successfully destroy'
+        redirect_to entities_url
+        flash[:notice]= 'entity was successfully destroy'
      end
       
 
